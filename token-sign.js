16e232d0be7fbcd02fc103c6a3e185ecc0a702f7
john code <john.code.877@gmail.com>
2024-10-23 21:49:35 -0500

    Se añade tiempo de expiracion al token

--- token-sign.js
+++ token-sign.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 
 const secret = process.env.JWT_SECRET
 
+const jwtConfig = {
+    //expiresIn: '7d',
+    expiresIn: '600000',// 10 minutos en milisegundos
+};
+
 const payload = {
    sub: 1, // identificador del token
    role: 'customer' 
    // scope es utilizado para los permisos  
 };
 
-function signToken(payload, secret) {   
-    return jwt.sign(payload, secret);
+function signToken(payload, secret,jwtConfig) {   
+    return jwt.sign(payload, secret,jwtConfig);
 }
 
 
-const token = signToken(payload,secret);
+const token = signToken(payload,secret,jwtConfig);
 
 console.log(token);
